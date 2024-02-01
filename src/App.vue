c7562ad1bd663a523d1feded8c780bee7859e21b
grossiValentina <grossivalentina435@gmail.com>
2024-02-01 15:40:16 +0100

    add:istallazione dipendenze, axios, bootstrap, sass

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script>
+export default{
+  data() {
+    return{
+
+    }
+  }
+}
 </script>
 
 <template>
+  <h1>hello</h1>
 </template>
 
-<style>
+<style lang="scss">
+@use "./styles/general.scss" as *
 </style>
