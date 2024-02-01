5366e348433d15692b1eb8a61ed28340612912e5
grossiValentina <grossivalentina435@gmail.com>
2024-02-01 16:35:45 +0100

    add: stampa dei dati in console

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,17 +4,17 @@ import axios from 'axios';
 export default{
   data() {
     return{
-
+        projects: []
     }
   },
   created(){
     axios.get('http://127.0.0.1:8000/api/projects')
     .then((resp) => {
         console.log(resp);
-        
+        this.projects = resp.data.result;
     })
-
   }
+
 }
 
 </script>
