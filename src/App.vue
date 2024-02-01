e0bc35356042abf9d3e91e6ea463e1e76f3b841c
grossiValentina <grossivalentina435@gmail.com>
2024-02-01 16:20:44 +0100

    add:chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ export default{
     }
   },
   components: 
-  {AppMain}
+  {AppMain},
 }
 </script>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,22 @@
 <script>
+import axios from 'axios';
+
 export default{
-  
+  data() {
+    return{
+
+    }
+  },
+  created(){
+    axios.get('http://127.0.0.1:8000/api/projects')
+    .then((resp) => {
+        console.log(resp);
+        
+    })
+
+  }
 }
+
 </script>
 
 <template>
