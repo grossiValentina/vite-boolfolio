f343432d5b7cfb8c18b02d1420989d9a3940ece9
grossiValentina <grossivalentina435@gmail.com>
2024-02-01 17:21:08 +0100

    add: visualizzazione delle card projects in pagina

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,28 +1,41 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
-export default{
-  data() {
-    return{
-        projects: []
-    }
-  },
-  created(){
-    axios.get('http://127.0.0.1:8000/api/projects')
-    .then((resp) => {
-        console.log(resp);
-        this.projects = resp.data.result;
-    })
-  }
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+
+    created() {
+        axios.get('http://127.0.0.1:8000/api/projects')
+            .then((resp) => {
+                //console.log(resp);
+                this.projects = resp.data.result;
+            })
+    },
+    
+    components: 
+     {ProjectCard},
 
 }
 
 </script>
 
 <template>
-  <h1>Main</h1>
+    
+    <h3 class="m-3 text-center">Lista dei Progetti:</h3>
+
+    <div class="container">
+        <div class="row row-cols-3 g-4">
+            <div v-for="project in projects" :key="project.id" class="col">
+                <!-- componente card -->
+                <ProjectCard :project="project"/>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+    props: {
+        project: Object,
+    },
+
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <h5 class="card-header bg-info text-white">{{ project.titolo }}</h5>
+        <div class="card-body">
+            <p class="fst-italic fw-lighter">{{ project.slug }}</p>
+            <p class="card-text">{{ project.descrizione }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
